336f874efe4b6eadc80f8364a169955f70926c62
Oleksandr <shcherbininalex81@gmail.com>
2026-04-13 20:56:25 +0300

    added utils next function

--- utils.py
+++ utils.py
@@ -1,2 +1,10 @@
 def add_two_numbers(num1: int | float, num2: int | float) -> int | float:
     return num1 + num2
+
+
+def sub_two_numbers(num1: int | float, num2: int | float) -> int | float:
+    return num1 - num2
+
+
+def multiply_two_numbers(num1: int | float, num2: int | float) -> int | float:
+    return num1 * num2
